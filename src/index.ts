6fa9ce8a4004ecafc3c78e0e8641cdc5ccb40fc3
jhonnatalima05-gif <jhonnatalima05@gmail.com>
2026-03-17 21:03:48 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,8 @@ app.get("/", async (req, res) => {
 app.use("/countries", countryRoutes);
 
 app.post("/webhook", (req, res) => {
-  const token = req.headers["x-token"];
-
-  if (token !== process.env.TICTO_TOKEN) {
-    console.log("❌ Token inválido");
-    return res.sendStatus(401);
-  }
-
-  console.log("🔥 WEBHOOK RECEBIDO:", req.body);
+  
+console.log("🔥 WEBHOOK RECEBIDO:", req.body);
   res.sendStatus(200);
 });
 
